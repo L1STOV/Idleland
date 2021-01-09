68e329c28f5b2293c23e24dd61068efb392894e5
L1STOV <listovspartak99@gmail.com>
2021-01-09 17:32:06 +0200

    Added test functions

--- test_character.py
+++ test_character.py
@@ -1,6 +1,23 @@
 import unittest
-from character import Character, Dwarf, Elf, Orc, Human
+from character import Dwarf, Elf, Orc, Human
 
 
 class TestCharacter(unittest.TestCase):
-    pass
+    def setUp(self):
+        dwain = Dwarf('Dwain', 100, 150, 100, 1500)
+        argalas = Elf('Argalas', 100, 100, 100, 2000)
+        urathun = Orc('Urathun', 100, 150, 125)
+        anduin = Human('Anduin', 100, 100, 90, 1250)
+
+    def test_dwarf_greetings(self):
+        pass
+
+    def test_elf_greetings(self):
+        pass
+
+    def test_orc_greetings(self):
+        pass
+
+    def test_human_greetings(self):
+        pass
+
