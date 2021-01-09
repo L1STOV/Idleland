bf1cb7549e28cb67f1be7c9af1889fb200f0d69b
L1STOV <listovspartak99@gmail.com>
2021-01-09 17:45:51 +0200

    Greetings functions tested

--- test_character.py
+++ test_character.py
@@ -4,20 +4,24 @@ from character import Dwarf, Elf, Orc, Human
 
 class TestCharacter(unittest.TestCase):
     def setUp(self):
-        dwain = Dwarf('Dwain', 100, 150, 100, 1500)
-        argalas = Elf('Argalas', 100, 100, 100, 2000)
-        urathun = Orc('Urathun', 100, 150, 125)
-        anduin = Human('Anduin', 100, 100, 90, 1250)
+        self.dwain = Dwarf('Dwain', 100, 150, 100, 1500)
+        self.argalas = Elf('Argalas', 100, 100, 100, 2000)
+        self.urathun = Orc('Urathun', 100, 150, 125)
+        self.anduin = Human('Anduin', 100, 100, 90, 1250)
 
     def test_dwarf_greetings(self):
-        pass
+        self.assertEqual(self.dwain.greetings(), 'Ho-ho, my name is Dwain. What are you staring at?')
 
     def test_elf_greetings(self):
-        pass
+        self.assertEqual(self.argalas.greetings(), 'Nae saian luume, my name is Argalas. I'
+                                                   ' was expecting to see someone bigger')
 
     def test_orc_greetings(self):
-        pass
+        self.assertEqual(self.urathun.greetings(), 'Bir ti gi dym, Urathun will kill you!')
 
     def test_human_greetings(self):
-        pass
+        self.assertEqual(self.anduin.greetings(), 'Greetings, my name is Anduin. How are you?')
 
+
+if __name__ == '__main__':
+    unittest.main()
