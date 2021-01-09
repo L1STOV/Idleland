adf1f10ad6220489068b9e7a3f6141006dba0963
L1STOV <listovspartak99@gmail.com>
2021-01-09 17:25:09 +0200

    Added abstract functions + implemented to child classes

--- character.py
+++ character.py
@@ -5,14 +5,27 @@ class Character:
         self.armor = armor
         self.force = force
 
+    def greetings(self):
+        raise NotImplementedError
+
+    def talk_to(self):
+        raise NotImplementedError
+
 
 class Dwarf(Character):
 
     amount_of_dwarfs = 0
 
-    def __init__(self, name, health, armor, force):
+    def __init__(self, name, health, armor, force, money):
         super().__init__(name, health, armor, force)
+        self.money = money
         Dwarf.amount_of_dwarfs += 1
+
+    def greetings(self):
+        pass
+
+    def talk_to(self):
+        pass
 
 
 class Orc(Character):
@@ -23,21 +36,40 @@ class Orc(Character):
         super().__init__(name, health, armor, force)
         Orc.amount_of_orcs += 1
 
+    def greetings(self):
+        pass
+
+    def talk_to(self):
+        pass
+
 
 class Elf(Character):
 
     amount_of_elfs = 0
 
-    def __init__(self, name, health, armor, force):
+    def __init__(self, name, health, armor, force, money):
         super().__init__(name, health, armor, force)
+        self.money = money
         Elf.amount_of_elfs += 1
+
+    def greetings(self):
+        pass
+
+    def talk_to(self):
+        pass
 
 
 class Human(Character):
 
     amount_of_humans = 0
 
-    def __init__(self, name, health, armor, force):
+    def __init__(self, name, health, armor, force, money):
         super().__init__(name, health, armor, force)
+        self.money = money
         Human.amount_of_humans += 1
-        
+
+    def greetings(self):
+        pass
+
+    def talk_to(self):
+        pass
