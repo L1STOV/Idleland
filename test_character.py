d1c6a834ab6de3030ab25d68524ab9bead1df143
L1STOV <listovspartak99@gmail.com>
2021-01-10 15:13:54 +0200

    Tests for set armor function for dward class created

--- test_character.py
+++ test_character.py
@@ -154,6 +154,41 @@ class TestCharacter(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.anduin.set_weapon({'statement': False}, 'Axe')
 
+    def test_dwarf_set_armor(self):
+        self.assertEqual(self.dwain.set_armor(50), '--- Current armor value - 200 points ---')
+
+    def test_dwarf_set_armor_banned_value(self):
+        self.assertEqual(self.dwain.set_armor(15), '--- Armor value fot dwarfs must be in [25:75] diapason ---')
+        self.assertEqual(self.dwain.set_armor(100), '--- Armor value fot dwarfs must be in [25:75] diapason ---')
+
+    def test_dwarf_set_armor_argument_type_str(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor('25')
+
+    def test_dwarf_set_armor_argument_type_float(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor(25.5)
+
+    def test_dwarf_set_armor_argument_type_none(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor(None)
+
+    def test_dwarf_set_armor_argument_type_bool(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor(True)
+
+    def test_dwarf_set_armor_argument_type_list(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor([25])
+
+    def test_dwarf_set_armor_argument_type_tuple(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor((25, 0))
+
+    def test_dwarf_set_armor_argument_type_dict(self):
+        with self.assertRaises(ValueError):
+            self.dwain.set_armor({'value': 25})
+
 
 if __name__ == '__main__':
     unittest.main()
